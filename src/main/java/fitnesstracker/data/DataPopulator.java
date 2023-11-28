cfc6c74767267287bee4b6b5896cbc4d2def50b2
Roney George <roney1969@gmail.com>
2023-11-28 11:10:05 +0000

    Person service and repository + populator

--- src/main/java/fitnesstracker/data/DataPopulator.java
+++ src/main/java/fitnesstracker/data/DataPopulator.java
@@ -1,11 +1,13 @@
 package fitnesstracker.data;
 
+import fitnesstracker.entities.Person;
 import fitnesstracker.entities.health.HealthStatistic;
 import fitnesstracker.entities.meal.Ingredient;
 import fitnesstracker.entities.meal.Meal;
 import fitnesstracker.repositories.HealthStatisticRepository;
 import fitnesstracker.services.HealthStatisticService;
 import fitnesstracker.services.MealService;
+import fitnesstracker.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
@@ -19,12 +21,14 @@ public class DataPopulator {
 
     private final MealService mealService;
     private final HealthStatisticService healthStatisticService;
+    private final PersonService personService;
 
 
     @Autowired
-    public DataPopulator(MealService mealService, HealthStatisticService healthStatisticService) {
+    public DataPopulator(MealService mealService, HealthStatisticService healthStatisticService, PersonService personService) {
         this.mealService = mealService;
         this.healthStatisticService = healthStatisticService;
+        this.personService = personService;
     }
 
     @EventListener(ContextRefreshedEvent.class)
@@ -50,9 +54,16 @@ public class DataPopulator {
         mealService.saveMeal(meal2);
         mealService.saveMeal(meal3);
 
-//        ******************************************************
+//*******************************************************************************
         LocalDate date = null;
         HealthStatistic healthStatistic1 = new HealthStatistic( date, 23.0,60.0, 80.0, 90.0,480.0, 620.0, 65.0, 10.0);
-        healthStatisticService.createHealthStatistic(healthStatistic1);
+        healthStatisticService.createHealthStatistic(healthStatistic1) ;
+        HealthStatistic healthStatistic2 = new HealthStatistic( date, 24.0,61.0, 81.0, 91.0,481.0, 621.0, 66.0, 11.0);
+        healthStatisticService.createHealthStatistic(healthStatistic2) ;
+//*******************************************************************************
+        Person person1 = new Person(1L, "Bob", "bob123", "password1");
+        personService.savePerson(person1);
+        Person person2 = new Person(2L, "Tom", "tom123", "password2");
+        personService.savePerson(person2);
     }
 }
